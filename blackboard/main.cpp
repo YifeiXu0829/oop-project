6a6573554dcb8c1488b1054dfba6674b46b7f572
Yifei Xu <yifeix@Yifeis-Air.hsd1.il.comcast.net>
2021-03-07 21:52:02 -0600

    update

--- blackboard/main.cpp
+++ blackboard/main.cpp
@@ -9,19 +9,19 @@ int main()
     while (1)
     {
         std::this_thread::sleep_for(std::chrono::milliseconds(1500));
-        std::string str_input = "string input";
+        std::string str_input{};
         bb.take_input(str_input);
 
         std::cout<<"=========================================================\n";
 
         std::this_thread::sleep_for(std::chrono::milliseconds(1500));
-        int32_t int_input = 0;
+        int32_t int_input{};
         bb.take_input(int_input);
 
         std::cout<<"=========================================================\n";
 
         std::this_thread::sleep_for(std::chrono::milliseconds(1500));
-        double double_input{0.0};
+        double double_input{};
         bb.take_input(double_input);
 
         std::cout<<"=========================================================\n";
