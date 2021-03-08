fae5086ee36416f307ceb9b6c9ccc7990b087df8
Yifei Xu <yifeix@Yifeis-Air.hsd1.il.comcast.net>
2021-03-07 21:48:31 -0600

    update

--- blackboard/blackboard.h
+++ blackboard/blackboard.h
@@ -3,6 +3,9 @@
 #include <iostream>
 #include "controller.h"
 
+struct solution_space{};
+struct central_data{};
+
 class blackboard
 {
 public:
@@ -18,4 +21,7 @@ public:
 
 
     controller ctrl_{*this};
+
+    central_data central_data_;
+    solution_space solution_space_;
 };
